fe31b7a63ff322c0a63a69f18c1995a18304a9a0
deokjoon.kang <dchkang83@gmail.com>
2024-12-06 11:57:27 +0900

    layout 및 page 테스트

--- frontend/source/app/layout.tsx
+++ frontend/source/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        main-layout
         {children}
       </body>
     </html>
