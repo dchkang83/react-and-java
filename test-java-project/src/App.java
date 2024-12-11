d7b4bc1acadf9549432b3f17c478bfc2481d3030
deokjoon.kang <dchkang83@gmail.com>
2024-12-11 12:14:49 +0900

    java 프로젝트 설정 및 jdk 설정

---
+++ test-java-project/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Java 버전 확인
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("Java Version: " + javaVersion);
+    }
+}
